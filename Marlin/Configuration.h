a36c4f9284f2cbe6b2451484c061e5cf7b7dffb6
fnsign <fotiosn@gmail.com>
2021-06-16 08:45:58 +0200

    added Bed Leveling temp preset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1838,11 +1838,11 @@
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
-//#define PREHEAT_2_LABEL       "ABS"
-//#define PREHEAT_2_TEMP_HOTEND 240
-//#define PREHEAT_2_TEMP_BED    110
-//#define PREHEAT_2_TEMP_CHAMBER 35
-//#define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
+#define PREHEAT_2_LABEL       "Bed Leveling"
+#define PREHEAT_2_TEMP_HOTEND 170
+#define PREHEAT_2_TEMP_BED    60
+#define PREHEAT_2_TEMP_CHAMBER 35
+#define PREHEAT_2_FAN_SPEED   0 // Value from 0 to 255
 
 /**
  * Nozzle Park
