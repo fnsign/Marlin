63abfebde82cc37580d634635cd4ab95a7929714
fnsign <fotiosn@gmail.com>
2021-08-16 14:13:54 +0200

    changed esteps to 135.37 (was 134.02)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -919,7 +919,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 134.02 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 135.37 }
 
 /**
  * Default Max Feed Rate (mm/s)
