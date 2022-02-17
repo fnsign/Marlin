67502acfdeee43fc9c2478f8d81b694cff39a87d
fnsign <fotiosn@gmail.com>
2022-02-17 13:32:43 +0100

    added PREHEAT for PETG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1893,11 +1893,17 @@
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "Bed Leveling"
-#define PREHEAT_2_TEMP_HOTEND 170
-#define PREHEAT_2_TEMP_BED    60
+#define PREHEAT_2_LABEL       "PETG"
+#define PREHEAT_2_TEMP_HOTEND 240
+#define PREHEAT_2_TEMP_BED     70
 #define PREHEAT_2_TEMP_CHAMBER 35
-#define PREHEAT_2_FAN_SPEED   0 // Value from 0 to 255
+#define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
+
+#define PREHEAT_3_LABEL       "Bed Leveling"
+#define PREHEAT_3_TEMP_HOTEND 170
+#define PREHEAT_3_TEMP_BED    60
+#define PREHEAT_3_TEMP_CHAMBER 35
+#define PREHEAT_3_FAN_SPEED   0 // Value from 0 to 255
 
 /**
  * Nozzle Park
